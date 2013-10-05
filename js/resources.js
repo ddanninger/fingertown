bb4ab88d62bf1128f42f2697909ca41ca32918f0
ddanninger <ddanninger@gmail.com>
2013-10-05 17:57:48 +0900

    * move TileSets to tilesets

--- js/resources.js
+++ js/resources.js
@@ -3,22 +3,22 @@ game.resources = [
     /**
      * Graphics.
      */
-    {name: "logo",  type:"image", src: "TitleSets/logo.png"},
-    {name: "startup",  type:"image", src: "TitleSets/startscreen.png"},
+    {name: "logo",  type:"image", src: "tilesets/logo.png"},
+    {name: "startup",  type:"image", src: "tilesets/startscreen.png"},
     {name: "32x32_font",          type:"image",	src: "font/32x32_font.png"},
     
-    {name: "loginBtn",  type:"image", src: "TitleSets/loginBtn.png"},
-    {name: "signupBtn",  type:"image", src: "TitleSets/signupBtn.png"},
+    {name: "loginBtn",  type:"image", src: "tilesets/loginBtn.png"},
+    {name: "signupBtn",  type:"image", src: "tilesets/signupBtn.png"},
     
     
  // our level tileset
-    {name: "tileset-shinygold-resized",  type:"image", src: "TitleSets/tileset-shinygold-resized.png"},
-    {name: "mainPlayer",  type:"image", src: "TitleSets/man_character.png"},
-    {name: "female",  type:"image", src: "TitleSets/girl_character.png"},
-    {name: "male",  type:"image", src: "TitleSets/man_character.png"},
-    {name: "govermentPlayer",  type:"image", src: "TitleSets/goverment_character.png"},
-    {name: "metatiles32x32",  type:"image", src: "TitleSets/metatiles32x32.png"},
-    {name: "treeentity",  type:"image", src: "TitleSets/treeentity.png"},
+    {name: "tileset-shinygold-resized",  type:"image", src: "tilesets/tileset-shinygold-resized.png"},
+    {name: "mainPlayer",  type:"image", src: "tilesets/man_character.png"},
+    {name: "female",  type:"image", src: "tilesets/girl_character.png"},
+    {name: "male",  type:"image", src: "tilesets/man_character.png"},
+    {name: "govermentPlayer",  type:"image", src: "tilesets/goverment_character.png"},
+    {name: "metatiles32x32",  type:"image", src: "tilesets/metatiles32x32.png"},
+    {name: "treeentity",  type:"image", src: "tilesets/treeentity.png"},
     
     
     
